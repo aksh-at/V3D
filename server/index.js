c4cb9df6e6194df4e1825cd318d8bdd3a01d60ee
stevenhao <stevenhao@gmail.com>
2017-07-29 16:12:29 -0700

    WIP

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
-var server = require('http').createServer();
-var io = require('socket.io')(server);
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+const views = {
+  main: {
+    online: false,
+    lastTime: null,
+    lastPoint: {
+      x: 0,
+      y: 0,
+    },
+  },
+  side: {
+    online: false,
+    lastTime: null,
+    lastPoint: {
+      x: 0,
+      y: 0,
+    },
+  },
+};
+
 io.on('connection', function(client){
+  let view = '';
   console.log('connection');
   client.on('frame', function(points){
     console.log('frame', points);
