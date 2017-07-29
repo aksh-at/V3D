145ba47c5c082e9c580688497854352d9ab00ee1
stevenhao <stevenhao@gmail.com>
2017-07-29 15:43:54 -0700

    add client side socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { Sketch } from './components';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
+const socket = io('http://localhost:3001');
+socket.emit('frame', [1, 2, 'hello']);
 class App extends Component {
   constructor() {
     super();
