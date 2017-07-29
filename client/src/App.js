ea2839743ea3c2ba6afce043998e231f2674b952
stevenhao <stevenhao@gmail.com>
2017-07-29 14:21:27 -0700

    add debug view

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      debug: false
+    }
+  }
+
   render() {
+    const { debug } = this.state;
+
     return (
-      <div className="App">
-        <div className="webcam">
-          <span>WEBCAM</span>
-        </div>
-        <div className="sketch">
-          SKETCH
-        </div>
-        <div className="tools">
-          TOOLS
+      <div
+        className={classnames({
+          "App": 1,
+          "dbg": debug,
+        })}>
+        <a
+          className={classnames({
+            'debug-link': 1,
+            'on': debug,
+          })}
+          onClick={() => {
+            this.setState({
+              debug: !debug
+            })
+          }} >
+          debug: { debug ? 'ON' : 'OFF'}
+        </a>
+        <div className="main">
+          <div className="webcam">
+            <span>WEBCAM</span>
+          </div>
+          <div className="sketch">
+            SKETCH
+          </div>
+          <div className="tools">
+            TOOLS
+          </div>
         </div>
       </div>
     );
