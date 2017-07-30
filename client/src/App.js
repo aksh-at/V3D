6586da92f92ddf7a40a4906be970dd56550ff71c
Catherine Li <catheryn@mit.edu>
2017-07-29 18:38:03 -0700

    Make home pretty + add buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { Sketch } from './components';
+import { Sketch } from './components/Sketch';
+import { Button } from './components/Button';
 import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
@@ -21,38 +22,90 @@ class App extends Component {
     }
   }
 
+  selectButton() {
+    return;
+  }
+
+  renderOptions() {
+    return (
+      <div>
+        <h4>Screen</h4>
+        <Button text="Main" selected={true} onSelect={this.selectButton}/>
+        <Button text="Side" selected={false} onSelect={this.selectButton}/>
+        <br />
+        <h4>Mode</h4>
+        <Button text="Trace" selected={false} onSelect={this.selectButton}/>
+        <Button text="Polyhedron" selected={true} onSelect={this.selectButton}/>
+        <Button text="Sphere" selected={false} onSelect={this.selectButton}/>
+      </div>
+    );
+  }
+
+  renderWebcam() {
+    return (
+      <span>
+        Lol webcam goes here
+      </span>
+    );
+  }
+
+  renderCanvas(items) {
+    return (
+      <Sketch items={items}/>
+    );
+  }
+
   render() {
     const { debug, items } = this.state;
 
     return (
-      <div
-        className={classnames({
-          "App": 1,
-          "dbg": debug,
-        })}>
-        <a
-          className={classnames({
-            'debug-link': 1,
-            'on': debug,
-          })}
-          onClick={() => {
-            this.setState({
-              debug: !debug
-            })
-          }} >
-          debug: { debug ? 'ON' : 'OFF'}
-        </a>
-        <div className="main">
-          <div className="webcam">
-            <span>WEBCAM</span>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-2 column">
+            <h3>Options</h3>
+            { this.renderOptions() }
           </div>
-          <Sketch items={items}/>
-          <div className="tools">
-            TOOLS
+          <div className="col-sm-4 column">
+            <h3>Webcam</h3>
+            { this.renderWebcam() }
+          </div>
+          <div className="col-sm-4 end-column">
+            <h3>Your Sketch</h3> 
+            { this.renderCanvas(items) }
           </div>
         </div>
       </div>
     );
+
+    // return (
+    //   <div
+    //     className={classnames({
+    //       "App": 1,
+    //       "dbg": debug,
+    //     })}>
+    //     <a
+    //       className={classnames({
+    //         'debug-link': 1,
+    //         'on': debug,
+    //       })}
+    //       onClick={() => {
+    //         this.setState({
+    //           debug: !debug
+    //         })
+    //       }} >
+    //       debug: { debug ? 'ON' : 'OFF'}
+    //     </a>
+    //     <div className="main">
+    //       <div className="webcam">
+    //         <span>WEBCAM</span>
+    //       </div>
+    //       <Sketch items={items}/>
+    //       <div className="tools">
+    //         TOOLS
+    //       </div>
+    //     </div>
+    //   </div>
+    // );
   }
 }
 
---
+++ client/src/components/Button.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export class Button extends Component {
+  	constructor(props) {
+	    super();
+	    this.state = {
+	    	selected: props.selected,
+	    };
+  	}
+
+  	render() {
+  		var classes = "btn";
+  		if (this.state.selected) {
+  			classes += " btn-info active"
+  		}
+  		return (
+  			<div className="option-button">
+
+  				<button type="button" className={classes}>
+  					{ this.props.text }
+  				</button>
+  			</div>
+  		);
+  	}
+
+  	selectButton() {
+  		this.setState({
+  			selected: true,
+  		})
+  	}
+
+  	deselectButton() {
+  		this.setState({
+  			selected: false,
+  		})
+  	}
+}
