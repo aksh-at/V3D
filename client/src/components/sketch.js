23d01577a0f42f9b136d4d8f3a8120d98a30a5bc
stevenhao <stevenhao@gmail.com>
2017-07-29 19:47:28 -0700

    react-three scaffolding

--- client/src/components/sketch.js
+++ client/src/components/sketch.js
@@ -1,5 +1,39 @@
 import React, { Component } from 'react';
 import { drawGrid, drawCircle } from '../canvas-utils';
+import ReactTHREE from 'react-three';
+import { PlaneBufferGeometry, Vector2, Vector3, ShaderMaterial } from 'three';
+
+var geometry = new PlaneBufferGeometry( 2, 2 );
+
+const { Renderer, Scene, Mesh, Object3d, PerspectiveCamera } = ReactTHREE;
+
+class Wavey extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.uniforms = {
+      time: { type: "f", value: props.time },
+      resolution: { type: "v2", value: new Vector2(props.width, props.height) }
+    }; 
+
+    this.material = new ShaderMaterial({
+      uniforms: this.uniforms,
+    });
+  }
+  componentWillReceiveProps(nextProps) {
+    this.uniforms.time.value = nextProps.time
+
+    if(nextProps.width !== this.props.width)
+      this.uniforms.resolution.value.x = nextProps.width;
+
+    if(nextProps.height !== this.props.height)
+      this.uniforms.resolution.value.y = nextProps.height;
+  }
+  render() {
+    return <Mesh geometry={geometry} material={this.material} />
+  }
+}
+
 
 export class Sketch extends Component {
   constructor() {
@@ -44,14 +78,25 @@ export class Sketch extends Component {
   }
 
   render() {
+    const width = 300;
+    const height = 300;
+    var aspectratio = width / height;
+    var cameraprops = {fov : 75, aspect : aspectratio, 
+      near : 1, far : 5000, 
+      position : new Vector3(0,0,600), 
+      lookat : new Vector3(0,0,0)};
+
     return (
       <div className="sketch">
-        <canvas
-          width="500"
-          height="500"
-          ref="canvas"
-        />
+        <Renderer width={width} height={height}>
+          <Scene width={width} height={height} camera="maincamera">
+            <PerspectiveCamera name="maincamera" {...cameraprops} />
+
+            <Wavey time={1.0} width={width} height={height} />
+          </Scene>
+        </Renderer>;
+        }
       </div>
     );
-  }
+}
 }
